ca160a4933c125c22ca6a8bf96a1c72df320bb01
rohitbundile-143 <rohitbundile@gmail.com>
2022-11-09 16:05:57 +0530

    Intial changes in suresh.py and roo.py

--- suresh.py
+++ suresh.py
@@ -20,3 +20,34 @@ def unpicklig():
         return data1
 u=unpicklig()
 u.method()
+
+A=iter([10,20,30,60])
+# for i in A:
+print(next(A))
+print(type(A))
+
+# def csv_file(roo_txt):
+#     for i in open('roo.txt','r'):
+#         if [x for x in data if x<len(50)]:
+#             yield i
+#             roo.txt.read().split('.')
+
+#
+# a=int(input('Enter no:'))
+# b=int(input('Enter no:'))
+# c=int(input('Enter no:'))
+# d=a**3+b**3+c**3
+# e=100*a+10*b+1*c
+# if d==e:
+#     print('This is armstrong no:',e)
+# else:
+#     print('This is  no armstrong :',e)
+#
+# def factorial(num):
+#     if num==0:
+#         fact=1
+#     else:
+#         fact=num*factorial(num-1)
+#     return fact
+# num=factorial(5)
+# print(num)
